aaaec52a99785835cf830ae08620a11ed28a1891
Albin Henningsson <albin.henningsson1@gmail.com>
2024-10-28 16:55:56 +0100

    init some structure, mostly research

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
+
+public struct HealthComponent
+{
+  int hp;
+}
+public struct PositionComponent
+{
+  float x;
+  float y;
+}
+
+public struct InputComponent
+{
+  Keyboard.Key[] keysPressed;
+  Keyboard.Key[] keysReleased;
+}
+
+public struct SpriteComponent
+{
+  Texture texture;
+
+}
+public struct VelocityComponent
+{
+  float x;
+  float y;
+}
+public struct ColliderComponent
+{
+  float width;
+  float height;
+}
+
+// need a way to query for entities that have certain components 
+public struct QueryResult
+{
+
+}
+
+public class ECS
+{
+
+
+  public QueryResult Query(int count, params int[] components)
+  {
+    return new();
+  }
+
+}
+
+public abstract class Entity
+{
+  public abstract void Create(ECS ecs);
+}
+
+public class Program
+{
+  public static int Main(string[] args)
+  {
+
+    return 0;
+  }
+}
