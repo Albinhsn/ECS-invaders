8c89b9d5e0fb2ba67fb95d6d47d132e4649a3500
Albinhsn <89841505+Albinhsn@users.noreply.github.com>
2025-01-03 06:53:00 +0100

    disable window resize

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Doing a basic invaders to teach myself ECS and win32
 
 # ToDo
-* Disallow resizing the window
 * Create a buffer that will be drawn each frame
 * Let every frame be 1/60 of a second
 * Allocate all the game memory 
--- src/win32_platform.c
+++ src/win32_platform.c
@@ -1,14 +1,14 @@
 #include "common.h"
-#include <stdio.h>
 #include <windows.h>
 
-static int   ScreenWidth  = 800;
-static int   ScreenHeight = 600;
+static s32   ScreenWidth  = 800;
+static s32   ScreenHeight = 600;
+static s32   ShouldQuit   = 0;
 
 static void* Win32_Allocate(u64 size)
 {
   // Align the memory?
-  return VirtualAlloc(0, size, MEM_RESERVE |MEM_COMMIT, PAGE_READWRITE);
+  return VirtualAlloc(0, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 }
 
 static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -17,6 +17,7 @@ static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
   {
   case WM_DESTROY:
   {
+    ShouldQuit = 1;
     PostQuitMessage(0);
     return 0;
   }
@@ -55,7 +56,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
   HWND hwnd = CreateWindowEx(0,                                                       // Optional window styles
                              WindowClassName,                                         // Window class
                              "Window Text",                                           // Window text
-                             WS_OVERLAPPEDWINDOW,                                     // Window style
+                             WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,                     // Window style, last part removes resizing
                              CW_USEDEFAULT, CW_USEDEFAULT, ScreenWidth, ScreenHeight, // Size and position
                              NULL,                                                    // Parent Window
                              NULL,                                                    // Menu
@@ -71,15 +72,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
   ShowWindow(hwnd, nCmdShow);
 
   // Allocate some amount of memory :)
-  void * Memory = Win32_Allocate(Gigabyte(1));
+  void* Memory = Win32_Allocate(Gigabyte(1));
 
-  // Game loop time
-
-  MSG Msg = {};
-  while (GetMessage(&Msg, NULL, 0, 0) > 0)
+  while (!ShouldQuit)
   {
-    TranslateMessage(&Msg);
-    DispatchMessage(&Msg);
+    MSG Msg = {};
+    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+      TranslateMessage(&Msg);
+      DispatchMessage(&Msg);
+    }
   }
 
   return 0;
